22ac6ad6403dcab380b385dd9fd5fd0587e1eabe
admin <m18820136354_1@163.com>
2019-08-29 16:08:55 +0800

    完成unit3学习

--- theFirstPart/part1-Scope-and-closure/unit3/04.js
+++ theFirstPart/part1-Scope-and-closure/unit3/04.js
@@ -91,3 +91,16 @@ if (foo) {
     console.log( baz );
   } 
 }
+
+
+
+//const
+var foo = true;
+if (foo) {
+  var a = 2;
+  const b = 3; // 包含在 if 中的块作用域常量
+  a = 3; // 正常!
+  b = 4; // 错误! 
+}
+console.log( a ); // 3
+console.log( b ); // ReferenceError!
--- theFirstPart/part1-Scope-and-closure/unit3/05B.js
+++ theFirstPart/part1-Scope-and-closure/unit3/05B.js
@@ -8,10 +8,17 @@
   }
 }
 console.log( a );//undefined
-// B.2 隐式和显式作用域
+// B.2 隐式和显式作用域    let-er 是一个构建时的代码转换器
 // 称作 let 作用域或 let 声明
 let (a = 2) {
   // 通过强制 性地将所有变量声明提升到块的顶部来产生更简洁的代码
   console.log( a ); // 2
+}
+console.log( a ); // ReferenceError
+
+/*let*/ 
+{ 
+  let a = 2; 
+  console.log( a );
 }
 console.log( a ); // ReferenceError
